22bf69fd7d30c462554b87aba90fcbf64539ef62
Aetibar Information & Technology <aetibar786@gmail.com>
2026-05-19 11:32:11 +0530

    Add additional URLs to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,5 +6,30 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: 'https://www.aetibar.in',
       lastModified: new Date(),
     },
+
+    {
+      url: 'https://www.aetibar.in/services',
+      lastModified: new Date(),
+    },
+
+    {
+      url: 'https://www.aetibar.in/projects',
+      lastModified: new Date(),
+    },
+
+    {
+      url: 'https://www.aetibar.in/process',
+      lastModified: new Date(),
+    },
+
+    {
+      url: 'https://www.aetibar.in/pricing',
+      lastModified: new Date(),
+    },
+
+    {
+      url: 'https://www.aetibar.in/contect',
+      lastModified: new Date(),
+    },
   ]
 }
