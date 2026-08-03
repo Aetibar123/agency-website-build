88770a69e7348224766ecd72104fcda57f228509
Aetibar Information & Technology <aetibar786@gmail.com>
2026-08-03 20:31:07 +0530

    Update TeamSection.tsx

--- src/components/about/TeamSection.tsx
+++ src/components/about/TeamSection.tsx
@@ -6,7 +6,7 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function TeamSection() {
   const team = [
-    { name: "Hasrat", role: "Chief Executive Officer", image: "" },
+    { name: "Hasrat", role: "Chief Executive Officer", image: "https://www.vectorstock.com/royalty-free-vectors/blank-profile-portrait-man-vectors" },
     { name: "Amaan", role: "Head of Engineering", image: "/images/about/person2.jpeg" },
     { name: "Aaqib", role: "Lead AI Architect", image: "/images/about/person1.jpeg" },
     { name: "Rohit", role: "Creative Director", image: "/images/about/person1.jpeg" },
