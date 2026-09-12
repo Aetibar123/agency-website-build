10ad30bad8ef5eda51c0ed2fe7e256a5cf65965d
Aetibar Information & Technology <aetibar786@gmail.com>
2026-09-13 00:26:41 +0530

    Refactor sitemap entries and adjust priorities
    Reorganize sitemap entries and update priorities.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -47,41 +47,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.8,
     },
-    {
-      url: `${baseUrl}/portfolio`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
+    
     {
       url: `${baseUrl}/blog`,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.8,
     },
-    {
-      url: `${baseUrl}/faq`,
+
+     {
+      url: `${baseUrl}/portfolio`,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: `${baseUrl}/careers`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.7,
-    },
-    {
-      url: `${baseUrl}/privacy-policy`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
-    },
-    {
-      url: `${baseUrl}/terms-of-service`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.3,
+      priority: 0.8,
     },
 
     ...servicePages,
